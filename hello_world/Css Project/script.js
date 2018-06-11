5e8c67feb6b0a76db352c121ab1faec55e334f37
Roshni Singh <Roshni.Singh@betsol.com>
2018-06-11 16:53:26 -0600

    CSS Practice

---
+++ hello_world/Css Project/script.js
@@ -0,0 +1,8 @@
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
+
+
+window.setTimeout(function () {
+    // put all of your JS code from the lecture here
+}, 500);
