c5382f843dc92ecf5b11b6073fd13d092fa4d909
Akyuna.Akish <Akyuna.Akish@BSL-DNV-L030>
2018-06-12 16:31:29 -0600

    started ejs project

--- ejs-express/routes/index.js
+++ ejs-express/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/fallinlovewith/:thing', function(req, res, next) {
+  res.render('love', { thingVar: req.params.thing });
 });
 
 module.exports = router;
