3d3f127d8c6ef0ffef5105e9d916c8b49c5aa92b
roshni singh <roshnisingh95@gmail.com>
2018-06-13 14:48:33 -0600

    EJS Express Practice

--- ejs-express/routes/index.js
+++ ejs-express/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require("body-parser");
+
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.use(express.static("public"));
+//router.set("view engine", "ejs");
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('home');
+});
+
+router.post("/addfriend", function(req, res){
+  var newFriend = req.body.newfriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
+});
+
+router.get("/friends", function (req, res) {
+  res.render("friends", { friends: friends });
 });
 
 router.get('/about', function(req, res, next) {
@@ -14,4 +32,16 @@ router.get('/fallinlovewith/:thing', function(req, res, next) {
   res.render('love', { thingVar: req.params.thing });
 });
 
+router.get("/posts", function(req, res){
+  var posts = [
+    { title: "Post1", author: "Susy"},
+    { title: "My adorable pet bunny", author: "Charlie" },
+    { title: "Can you believe this pomsky", author: "Colt" }
+  ];
+
+  res.render("posts.ejs", {posts: posts});
+});
+
+
 module.exports = router;
+
